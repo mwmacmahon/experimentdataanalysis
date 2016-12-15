2352e0ddb5bab239d650753b9cf51fb4133ba97e
Michael Macmahon <mwmacmahon@gmail.com>
2016-12-15 00:37:42 -0500

    minor changes to simulation

--- script_discrete_channel_simulation.py
+++ script_discrete_channel_simulation.py
@@ -8,6 +8,9 @@ Created on Thu Dec  1 17:53:06 2016
 import matplotlib.pyplot as plt
 import numpy as np
 
+from experimentdataanalysis.analysis.generalutilities \
+    import multiprocessable_map
+
 GFACTORCONSTANT = 0.013996  # 1/(ps*Tesla), = bohr magneton/2*pi*hbar
 LASER_REPRATE = 13160  # ps period
 
@@ -26,7 +29,7 @@ def toy_kerr_rotation_curve(x_to_test):
     return np.interp(x_to_test, x, y)
 
 
-def generate_absorption_operator(band_edge, relative_phase):
+def generate_absorption_operator(band_edge, relative_phase, relative_amp):
     """
     Generates a function that takes a spin polarization & orientation profile,
     a laser intensity profile, and that laser's wavelength, then returns
@@ -34,11 +37,14 @@ def generate_absorption_operator(band_edge, relative_phase):
     kronig blah blah rules and stuff ideally. Takes as parameters the
     band edge of the target spin species, and any other parameters needed
     to inform how the absorption should play out.
+    
+    relative_phase, relative_amp are standins for actual band edge-based
+    calculations.
     """
-    def absorption_operator(complex_spin_profile,
+    def absorption_operator(complex_spin_profile, external_b_field,
                             laser_intensity_profile, laser_wavelength):
         # TODO: insert complexities of adding the new pulse!
-        complex_spin_profile += laser_intensity_profile * \
+        complex_spin_profile += laser_intensity_profile * relative_amp * \
                                     np.exp(1j * relative_phase)
 #        if laser_wavelength > band_edge:
 #            complex_spin_profile += laser_intensity_profile  # on-axis = real
@@ -49,7 +55,7 @@ def generate_absorption_operator(band_edge, relative_phase):
     return absorption_operator
 
 
-def generate_kerr_rotation_operator(band_edge, relative_phase):
+def generate_kerr_rotation_operator(band_edge, relative_phase, relative_amp):
     """
     Generates a function that takes a spin polarization & orientation profile,
     a laser intensity profile, and that laser's wavelength, then returns
@@ -57,8 +63,11 @@ def generate_kerr_rotation_operator(band_edge, relative_phase):
     kronig beep boop stuff ideally. Takes as parameters the band edge
     of the target spin species, and any other parameters needed
     to inform how the kerr rotation should play out.
+
+    relative_phase, relative_amp are standins for actual band edge-based
+    calculations.
     """
-    def kerr_rotation_operator(complex_spin_profile,
+    def kerr_rotation_operator(complex_spin_profile, external_b_field,
                                laser_intensity_profile, laser_wavelength):
         # TODO: fancy wavelength and band_edge dependent stuff, even
         # probe-wavelength dependent stuff for a real show off
@@ -67,7 +76,7 @@ def generate_kerr_rotation_operator(band_edge, relative_phase):
 #        complex_spin_profile *= np.exp(1j * -1 * relative_phase)
         measured_profile = np.real(complex_spin_profile)
         probe_convolution = laser_intensity_profile * measured_profile
-        signal = sum(probe_convolution)
+        signal = sum(probe_convolution) * relative_amp
         return signal
     return kerr_rotation_operator
 
@@ -103,20 +112,22 @@ class SpinProfile():
         self.orientation = np.angle(complex_profile)
         self.orientation = ((self.orientation + np.pi/2) % (2*np.pi)) - np.pi/2
 
-    def generate_packet(self, laser_wavelength,
+    def generate_packet(self, external_b_field, laser_wavelength,
                         laser_power, laser_width, laser_position):
         laser_intensity_profile = laser_power * \
             np.exp(-(self.xvals - laser_position)**2 / (2*laser_width**2))
         new_complex_spin_profile = \
             self.absorption_operator(self.get_complex_spin_profile(),
+                                     external_b_field,
                                      laser_intensity_profile, laser_wavelength)
         self.set_complex_spin_profile(new_complex_spin_profile)
 
-    def probe_at_position(self, laser_wavelength,
+    def probe_at_position(self, external_b_field, laser_wavelength,
                           laser_power, laser_width, laser_position):
         laser_intensity_profile = laser_power * \
             np.exp(-(self.xvals - laser_position)**2 / (2*laser_width**2))
         return self.kerr_rotation_operator(self.get_complex_spin_profile(),
+                                           external_b_field,
                                            laser_intensity_profile,
                                            laser_wavelength)
 
@@ -165,36 +176,24 @@ class SpinProfile():
 
 
 # %%
-if __name__ == "__main__":
-    # TO KEEP FLOW SIMPLE, FLOW WILL ONLY OCCUR IN INTEGER SPATIAL STEPS
-    # THUS HIGH TIME RESOLUTION NECESSITATES HIGH SPATIAL RESOLUTION!
-
-    # general parameters
-    xmin, xmax = (-100, 250)  # um
-    nboxes = 350
-    num_pulses = 10
-    time_step = 10.0  # ps
-
-    # field parameters
-    electric_field = 15  # V/cm
-    external_b_field = 0.3
-
-    # recording parameters
-    surpress_plot = False
-    if not surpress_plot:
-        plt.figure()
-        pos_graph_axes = plt.subplot(3,1,1)
-        trkr_graph_axes_list = [plt.subplot(3,2,3),
-                                plt.subplot(3,2,4),
-                                plt.subplot(3,2,5),
-                                plt.subplot(3,2,6)]
-    probe_position_list = [-10, 0, 20, 30]    
-    measurement_window = [-500, 7000]
-    time_step = 5.0  # ps
-    streak_start = -500
-    streak_end = 7000
+def model_spin_channels(electric_field,  # V/cm
+                        external_b_field,
+                        gen_params_dict,
+                        pump_laser_kwargs,
+                        probe_laser_kwargs,
+                        species1_kwargs,
+                        species2_kwargs,
+                        suppress_plot=False):
+    
+    num_pulses = gen_params_dict['num_pulses']
+    time_step = gen_params_dict['time_step']
+    probe_position_list = gen_params_dict['probe_position_list']
+    measurement_window = gen_params_dict['measurement_window']
+    streak_start = gen_params_dict['streak_start']
+    streak_end = gen_params_dict['streak_end']
+    streak_line_count = gen_params_dict['streak_line_count']
+                        
     streak_duration = streak_end - streak_start
-    streak_line_count = 100  # lines in streak, give or take one, limited by time step
     streak_spacing = streak_duration // streak_line_count
     steps_per_pulse = int(round(LASER_REPRATE/time_step))
     lab_time = -(num_pulses - 1)*LASER_REPRATE  # lab_time=0 is last pulse
@@ -204,35 +203,9 @@ if __name__ == "__main__":
                         for num in range(len(probe_position_list))]
     timeseries_current_index = 0
 
-    # laser parameters
-    pump_laser_kwargs = dict(laser_wavelength=818.9,  # nm
-                             laser_power=1.0,  # AU
-                             laser_width=20.0,  # um
-                             laser_position=0.0)  # um
-    probe_laser_kwargs = dict(laser_wavelength=818.9,  # nm
-                              laser_power=1.0,  # AU
-                              laser_width=20.0)  # um
-                              # add position manually!
-
     # create species profiles, will evolve alongside each other
-    species1 = SpinProfile(xmin, xmax, nboxes,
-                           carrier_density=1.0,
-                           spin_lifetime=20000.0,  # ps
-                           gfactor=.44,
-                           mobility=1e-4,  # (um/ps)/(V/cm). 1e-4 -> 2um/ns/Vapp
-                           transfer_decay_const=1e-8,
-                           diffusion_const=0,
-                           band_edge=818.4,
-                           relative_phase=0*np.pi)
-    species2 = SpinProfile(xmin, xmax, nboxes,
-                           carrier_density=2.0,
-                           spin_lifetime=2000.0,  # ps
-                           gfactor=.436,
-                           mobility=1e-4,  # (um/ps)/(V/cm). 1e-4 -> 2um/ns/Vapp
-                           transfer_decay_const=1e-8,
-                           diffusion_const=0,
-                           band_edge=819.4,
-                           relative_phase=-2*np.pi/3)
+    species1 = SpinProfile(**species1_kwargs)
+    species2 = SpinProfile(**species2_kwargs)
 
     def time_evolve_system():
         species1_transferred_complex_spin_profile = \
@@ -246,9 +219,18 @@ if __name__ == "__main__":
             species2.get_complex_spin_profile() +
             species1_transferred_complex_spin_profile)
         
+    # recording parameters
+    if not suppress_plot:
+        plt.figure()
+        pos_graph_axes = plt.subplot(3,1,1)
+        trkr_graph_axes_list = [plt.subplot(3,2,3),
+                                plt.subplot(3,2,4),
+                                plt.subplot(3,2,5),
+                                plt.subplot(3,2,6)]
+
     for i in range(num_pulses):
-        species1.generate_packet(**pump_laser_kwargs)
-        species2.generate_packet(**pump_laser_kwargs)
+        species1.generate_packet(external_b_field, **pump_laser_kwargs)
+        species2.generate_packet(external_b_field, **pump_laser_kwargs)
         for j in range(steps_per_pulse):  # time steps per pulse
             time_evolve_system()
             # compute and record channel state if necessary
@@ -256,7 +238,7 @@ if __name__ == "__main__":
             streak_time = lab_time - streak_start
             record_trkr = all([lab_time >= measurement_window[0],
                                lab_time <= measurement_window[1]])
-            record_streak = all([not surpress_plot,
+            record_streak = all([not suppress_plot,
                                  streak_time >= 0,
                                  streak_time < streak_duration,
                                  streak_time % streak_spacing < time_step])
@@ -272,7 +254,7 @@ if __name__ == "__main__":
 #                                                laser_position=x)
 #                     for x in species1.xvals])
                 sum_profile = profile1 + profile2
-            if record_streak and not surpress_plot:
+            if record_streak and not suppress_plot:
                 brightness = 1 - (streak_time / streak_duration)**4.0
                 if brightness > 1 or brightness < 0:
                     raise Exception("error: invalid brightness value")
@@ -283,9 +265,11 @@ if __name__ == "__main__":
 #                    pos_index = np.argwhere(species1.xvals > pos)[0]
 #                    series_val = sum_profile[pos_index]
                     series_val = \
-                        species1.probe_at_position(**probe_laser_kwargs,
+                        species1.probe_at_position(external_b_field,
+                                                   **probe_laser_kwargs,
                                                    laser_position=pos) + \
-                        species2.probe_at_position(**probe_laser_kwargs,
+                        species2.probe_at_position(external_b_field,
+                                                   **probe_laser_kwargs,
                                                    laser_position=pos)
                     series = trkr_vs_pos_list[list_ind]
                     if timeseries_current_index < len(series):
@@ -296,9 +280,9 @@ if __name__ == "__main__":
                 timeseries_current_index += 1
 
     # Plot results
-    if not surpress_plot:
-        times = np.linspace(measurement_window[0],
-                            measurement_window[1], num_time_indices)
+    times = np.linspace(measurement_window[0],
+                        measurement_window[1], num_time_indices)
+    if not suppress_plot:
         posymin, posymax = pos_graph_axes.yaxis.get_view_interval()
         for posnum in range(4):
             axes = trkr_graph_axes_list[posnum]
@@ -312,6 +296,8 @@ if __name__ == "__main__":
             pos_graph_axes.plot(2*[probe_position_list[posnum]],
                                 [posymin, posymax - .001], 'r')           
 
+    return times, trkr_vs_pos_list
+
 #    plt.subplot(2,1,2)
 #    plt.plot(1 + species1.polarization, 'r:')
 #    plt.plot(1 + species1.orientation / np.pi, 'b:')
@@ -319,5 +305,127 @@ if __name__ == "__main__":
 #    plt.plot(2 + species2.orientation / np.pi, 'b:')
 #    plt.plot(species1.polarization * np.cos(species1.orientation) +
 #             species2.polarization * np.cos(species2.orientation), 'gd')
+
+
+# %%
+if __name__ == "__main__":
+    # TO KEEP FLOW SIMPLE, FLOW WILL ONLY OCCUR IN INTEGER SPATIAL STEPS
+    # THUS HIGH TIME RESOLUTION NECESSITATES HIGH SPATIAL RESOLUTION!
+
+    # general parameters
+    xmin, xmax = (-100, 250)  # um
+    nboxes = 350
+    num_pulses = 15
+    time_step = 5.0  # ps
+    probe_position_list = [-10, 0, 10, 20]    
+    measurement_window = [-500, 7000]
+    streak_start = -500
+    streak_end = 7000
+    streak_line_count = 100  # lines in streak, give or take one, limited by time step
+    gen_params_dict = dict(num_pulses=num_pulses,
+                           time_step=time_step,
+                           probe_position_list=probe_position_list,
+                           measurement_window=measurement_window,
+                           streak_start=streak_start,
+                           streak_end=streak_end,
+                           streak_line_count=streak_line_count)
+
+    # field parameters
+    electric_field = 15  # V/cm
+    external_b_field = 0.3
+
+    # laser parameters
+    pump_laser_kwargs = dict(laser_wavelength=818.9,  # nm
+                             laser_power=1.0,  # AU
+                             laser_width=20.0,  # um
+                             laser_position=0.0)  # um
+    probe_laser_kwargs = dict(laser_wavelength=818.9,  # nm
+                              laser_power=1.0,  # AU
+                              laser_width=20.0)  # um
+                              # add position manually!
+
+    species1_kwargs = dict(xmin=xmin,
+                           xmax=xmax,
+                           nboxes=nboxes,
+                           carrier_density=1.0,
+                           spin_lifetime=20000.0,  # ps
+                           gfactor=.44,
+                           mobility=1e-4,  # (um/ps)/(V/cm). 1e-4 -> 2um/ns/Vapp
+                           transfer_decay_const=1e-7,
+                           diffusion_const=0,
+                           band_edge=818.4,
+                           relative_phase=0*np.pi,
+                           relative_amp=1.0)
+
+    species2_kwargs = dict(xmin=xmin,
+                           xmax=xmax,
+                           nboxes=nboxes,
+                           carrier_density=1.0,
+                           spin_lifetime=2000.0,  # ps
+                           gfactor=.44,
+                           mobility=1e-4,  # (um/ps)/(V/cm). 1e-4 -> 2um/ns/Vapp
+                           transfer_decay_const=1e-7,
+                           diffusion_const=0,
+                           band_edge=819.4,
+                           relative_phase=-3*np.pi/3,
+                           relative_amp=2.0)
+
+# %%
+    suppress_plot = True  # for individual simulations
+    posind_to_plot = 1
+
+    n_b_fields = 3
+    b_field_list = np.linspace(0.15, 0.3, n_b_fields)
+    n_relative_amps = 4
+    relative_amp_list = np.linspace(1.5, 3.0, n_relative_amps)
+
+    # get data
+    input_arglist_list = []
+    for relative_amp in relative_amp_list:
+        for b_field in b_field_list:
+            species2_kwargs['relative_amp'] = relative_amp
+            input_arglist_list.append([electric_field,  # V/cm
+                                       b_field,
+                                       gen_params_dict.copy(),
+                                       pump_laser_kwargs.copy(),
+                                       probe_laser_kwargs.copy(),
+                                       species1_kwargs.copy(),
+                                       species2_kwargs.copy(),
+                                       suppress_plot])
+
+    output_list = multiprocessable_map(model_spin_channels,
+                                       input_arglist_list,
+                                       multiprocessing=True)
+    output_iter = iter(output_list)
+    yvals_vs_pos_vs_field_vs_relative_amp = []
+    for i in range(n_relative_amps):
+        yvals_vs_pos_vs_field = []
+        for j in range(n_b_fields):
+            times, yvals_vs_pos = next(output_iter)
+            yvals_vs_pos_vs_field.append(yvals_vs_pos)
+        yvals_vs_pos_vs_field_vs_relative_amp.append(yvals_vs_pos_vs_field)
+
+    # plot
+    plt.figure()
+    for amp_ind, amp in enumerate(relative_amp_list):
+        yvals_vs_pos_vs_field = \
+            yvals_vs_pos_vs_field_vs_relative_amp[amp_ind]
+        for b_ind, b_field in enumerate(b_field_list):
+            yvals = yvals_vs_pos_vs_field[b_ind][posind_to_plot]
+            axes = plt.subplot(n_b_fields, n_relative_amps,
+                               b_ind * n_relative_amps + amp_ind + 1)
+            axes.plot(times, yvals)
+            axes.text(0.5, 0.9,
+                      "TRKR @x={}".format(probe_position_list[posind_to_plot]),
+                      horizontalalignment='center', verticalalignment='center',
+                      fontsize=18, transform=axes.transAxes)
+            axes.set_yticklabels([])
+#            plt.title("b: {}, amp: {}".format(b_ind, amp_ind))
+            if amp_ind == 0:
+                plt.ylabel("B = {:.3f}T".format(b_field), fontsize=24)
+            if b_ind == 0:
+                plt.title("absorb_ratio = {:.1f}".format(amp),
+                          fontsize=20)
+
 
 
